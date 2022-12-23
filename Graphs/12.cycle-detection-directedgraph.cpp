edac04fd08c26650d33a81c653911c23ee551e08
lalitkashyap58 <77583908+lalitkashyap58@users.noreply.github.com>
2022-12-23 19:49:21 +0530

    https://practice.geeksforgeeks.org/problems/detect-cycle-in-a-directed-graph/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=detect-cycle-in-a-directed-graph

---
+++ Graphs/12.cycle-detection-directedgraph.cpp
@@ -0,0 +1,71 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+ private:
+    bool checkCycle(int node, vector < int > adj[], int vis[], int dfsVis[]) {
+      vis[node] = 1;
+      dfsVis[node] = 1;
+      for (auto it: adj[node]) {
+        if (!vis[it]) {
+          if (checkCycle(it, adj, vis, dfsVis)) return true;
+        } else if (dfsVis[it]) {
+          return true;
+        }
+      }
+      dfsVis[node] = 0;
+      return false;
+    }
+  public:
+    bool isCyclic(int N, vector < int > adj[]) {
+      int vis[N], dfsVis[N];
+     
+      for(int i = 0; i < N; i++){
+        vis[i] = 0;
+        dfsVis[i] = 0;
+      }
+
+      for (int i = 0; i < N; i++) {
+        if (!vis[i]) {
+          // cout << i << endl; 
+          if (checkCycle(i, adj, vis, dfsVis)) {
+             // cout << i << endl;
+            return true;
+          }
+        }
+      }
+      return false;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+
+    int t;
+    cin >> t;
+    while (t--) {
+        int V, E;
+        cin >> V >> E;
+
+        vector<int> adj[V];
+
+        for (int i = 0; i < E; i++) {
+            int u, v;
+            cin >> u >> v;
+            adj[u].push_back(v);
+        }
+
+        Solution obj;
+        cout << obj.isCyclic(V, adj) << "\n";
+    }
+
+    return 0;
+}
+
+// } Driver Code Ends
