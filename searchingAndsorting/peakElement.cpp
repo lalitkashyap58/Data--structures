75894826f66bad34eab57660eaf3bc3e2e289c45
lalitkashyap58 <77583908+lalitkashyap58@users.noreply.github.com>
2023-01-26 00:43:24 +0530

    https://leetcode.com/problems/find-peak-element/description/
    one of the important question from binary search

---
+++ searchingAndsorting/peakElement.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {int low=0;
+                                            int n=nums.size();
+                                            int high=n-1;
+                                            
+                                            while(low<=high)
+                                            {int mid=(low+high)/2;
+                                                if((mid==0||nums[(mid-1)]<=nums[mid])&&(mid==n-1||nums[mid+1]<=nums[mid]))
+                                                   return mid;
+                                                   if(mid>0&&nums[mid-1]>=nums[mid])
+                                                   high=mid-1;
+                                                   else
+                                                   low=mid+1;
+                                            }
+                                                   return -1;
+        
+    }
+};
